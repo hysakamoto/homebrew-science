3dd2bc74f06220b178859b4e1acdfe1a8525560b
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-03-06 21:44:50 -0600

    audited

--- superlu.rb
+++ superlu.rb
@@ -5,27 +5,26 @@ class Superlu < Formula
   url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz"
   sha1 "d2863610d8c545d250ffd020b8e74dc667d7cbdd"
 
-  # depends_on "cmake" => :build
   depends_on :fortran
   depends_on 'openblas' => :recommended
 
-  def install    
+  def install
     ENV.deparallelize
     system "cp MAKE_INC/make.mac-x ./make.inc"
-    inreplace 'make.inc', '$(HOME)/Codes/SuperLU_4.3', "#{buildpath}"
+    inreplace 'make.inc', '$(HOME)/Codes/SuperLU_4.3', buildpath
     inreplace 'make.inc', 'ranlib', 'echo'
 
     if build.with? 'openblas'
-      inreplace "make.inc", 
-      "$(SuperLUroot)/lib/libblas.a", 
+      inreplace "make.inc",
+      "$(SuperLUroot)/lib/libblas.a",
       "-L#{Formula["openblas"].lib} -lopenblas"
     else
       system "make blaslib"
     end
 
     system "make"
-    
-    inreplace 'make.inc', "#{buildpath}", "#{prefix}"
+
+    inreplace 'make.inc', buildpath, prefix
     prefix.install Dir["*"]
 
   end
@@ -36,7 +35,7 @@ class Superlu < Formula
       system "make"
       out = `grep passed #{prefix}/TESTING/*.out`
       print out
-      ohai "All the test outputs are in #{prefix}/TESTING/*.out. Please check."      
+      ohai "All the test outputs are in #{prefix}/TESTING/*.out. Please check."
     end
   end
 

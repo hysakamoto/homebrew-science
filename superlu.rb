68d7fd00bbeca5b321c6588a74b8760ea85f0049
Yusuke Sakamoto <yus.sakamoto@gmail.com>
2014-03-07 10:00:26 -0600

    added Accelerate framework support testing updated inreplace updated

--- superlu.rb
+++ superlu.rb
@@ -6,37 +6,35 @@ class Superlu < Formula
   sha1 "d2863610d8c545d250ffd020b8e74dc667d7cbdd"
 
   depends_on :fortran
-  depends_on 'openblas' => :recommended
+  depends_on 'openblas' => :optional
 
   def install
     ENV.deparallelize
-    system "cp MAKE_INC/make.mac-x ./make.inc"
-    inreplace 'make.inc', '$(HOME)/Codes/SuperLU_4.3', buildpath
-    inreplace 'make.inc', 'ranlib', 'echo'
+    cp "MAKE_INC/make.mac-x", "./make.inc"
 
-    if build.with? 'openblas'
-      inreplace "make.inc",
-      "$(SuperLUroot)/lib/libblas.a",
-      "-L#{Formula["openblas"].lib} -lopenblas"
-    else
-      system "make blaslib"
+    inreplace 'make.inc' do |s|
+      s.gsub! "$(HOME)/Codes/SuperLU_4.3", buildpath
+      s.gsub! "ranlib", "echo"
+      if build.with? 'openblas'
+        s.gsub! "$(SuperLUroot)/lib/libblas.a", "-L#{Formula["openblas"].lib} -lopenblas"
+      else
+        s.gsub! "$(SuperLUroot)/lib/libblas.a", "-framework Accelerate"
+      end
     end
 
     system "make"
-
     inreplace 'make.inc', buildpath, prefix
     prefix.install Dir["*"]
 
   end
 
   test do
-    cd "#{prefix}/TESTING" do
-      ENV.deparallelize
-      system "make"
-      out = `grep passed #{prefix}/TESTING/*.out`
-      print out
-      ohai "All the test outputs are in #{prefix}/TESTING/*.out. Please check."
-    end
+    cp_r "#{prefix}/TESTING", testpath
+    cd "#{prefix}/TESTING" 
+    ENV.deparallelize
+    system "make"
+    assert File.exist?("dtest.out") and File.exist?("ztest.out")
+    assert (not File.read("dtest.out").include?("fail"))
   end
 
 end
